f03db6ac7513c050e6b6add6161f2831b6279b04
Reindra Antoni <reindraantoni@gmail.com>
2024-08-06 19:17:04 +0700

    .

--- src/app/(portofolio)/page.tsx
+++ src/app/(portofolio)/page.tsx
@@ -7,14 +7,12 @@ import Hero from "@/components/main/hero";
 import About from "@/components/main/about";
 import Projects from "@/components/main/projects";
 import Contact from "@/components/main/contact";
-import Header from "@/components/main/header";
-import Footer from "@/components/main/footer";
 
 export default function Home() {
   return (
     <main className="w-full h-full">
       <FloatingNav navItems={navItems} />
-      <div className="flex flex-col gap-40 px-10"> 
+      <div className="flex flex-col gap-80 px-10"> 
         <Hero />  
         <About />
         <Projects />
